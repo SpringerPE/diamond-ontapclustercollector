fa4ab61d1bc9fd3d9aa8466916407d1462a91cb2
Jose Riguera <jriguera@gmail.com>
2015-07-20 20:46:24 +0200

    improvements

--- docker/confd.sh
+++ docker/confd.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-if [ ! -z "$CONFD_BIN" ] && [ -x "$CONFD_BIN" ]; then
+function alarm() {
+    local timeout=$1; shift;
+    bash -c "$@" &
+    local pid=$!
+    {
+      sleep $timeout
+      kill $pid 2> /dev/null
+    } &
+    wait $pid 2> /dev/null
+    return $?
+}
+
+if command -v confd >/dev/null 2>&1; then
    ETCD_PORT=${ETCD_PORT:-4001}
    ETCD_HOST=${ETCD_HOST:-10.1.42.1}
 
    # Check if the ETCD host:port is open
-   exec 6<>/dev/tcp/$ETCD_HOST/$ETCD_PORT
+   #exec 6<>/dev/tcp/$ETCD_HOST/$ETCD_PORT
+   #RESULT=$?
+   #exec 6>&- # close output connection
+   #exec 6<&- # close input connection
+   alarm 3 "echo > /dev/tcp/$ETCD_HOST/$ETCD_PORT"
    RESULT=$?
-   exec 6>&- # close output connection
-   exec 6<&- # close input connection
 
    if [ $RESULT == 0 ]; then
       # Run confd in the background to watch the upstream servers
-      exec $CONFD_BIN -node "http://$ETCD_HOST:$ETCD_PORT"
+      exec confd -node "http://$ETCD_HOST:$ETCD_PORT"
    else
       # Run confd in the background to watch the upstream servers
-      exec $CONFD_BIN -backend env
+      exec confd -backend env
    fi
 fi
 exec true
--- docker/diamond.sh
+++ docker/diamond.sh
@@ -4,8 +4,5 @@ if [ ! -d /var/log/diamond ]; then
     mkdir /var/log/diamond
 fi
 
-# Configure OntaClusterCollector
-/usr/local/bin/diamond-ontap-configurator.sh add ${NETAPP_HOST} ${NETAPP_IP} ${NETAPP_USER} ${NETAPP_PASSWORD}
-
 # Launch Diamond
 exec /usr/local/bin/diamond -f --skip-change-user --skip-fork -c /etc/diamond/diamond.conf
