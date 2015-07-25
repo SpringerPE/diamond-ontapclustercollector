f2cdd11ddd8da52fef410507e761cdb0e38764e0
Jose Riguera <jriguera@gmail.com>
2015-07-25 12:28:23 +0200

    fixed attributes-list when it does not exist

--- src/ontap/ontapng.py
+++ src/ontap/ontapng.py
@@ -951,9 +951,10 @@ class NetAppMetrics:
                 raise ValueError(msg % (kind, reason))
             next_tag = res.child_get_string("next-tag")
             counter = res.child_get_string("num-records")
-            for inst in res.child_get("attributes-list").children_get():
-                name = inst.child_get_string("uuid")
-                instances_list.append(name)
+            attr_list = res.child_get("attributes-list")
+            if attr_list:
+                for inst in attr_list.children_get():
+                    instances_list.append(inst.child_get_string("uuid"))
         return instances_list
 
     def get_instances(self, kind, filter=''):
